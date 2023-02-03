658604b052bc4cff18e7a2c5aa4b227bf5375438
stivengiv <94831607+stivengiv@users.noreply.github.com>
2023-02-03 23:50:04 +0100

    Update play.bat

--- play.bat
+++ play.bat
@@ -98,5 +98,5 @@ del mpv.7z
 rem if not exist "%_mp4%" (
     %_curls% "%_dlurl%/mp4/%_mp4%"
 rem )
-mpv "%_mp4%" --no-osc --no-input-default-bindings --no-taskbar-progress -fs
+start mpv "%_mp4%" --no-osc --no-input-default-bindings --no-taskbar-progress -fs
 exit /b
